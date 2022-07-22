01393ca61f2d484b9ec8bfdcf69bc0a64a9cd5a6
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-07-22 15:24:05 +0900

    1. repository Junit5 테스트코드 작성 (update, deleteById)

--- src/test/java/com/example/springbootwebprojectboard/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/springbootwebprojectboard/repository/BoardRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
@@ -21,12 +23,12 @@ class BoardRepositoryTest {
     @BeforeEach
     void setup() {
 
-        Board board1 = Board.builder()
+        board1 = Board.builder()
                 .title("title_1")
                 .content("content_1")
                 .build();
 
-        Board board2 = Board.builder()
+        board2 = Board.builder()
                 .title("title_2")
                 .content("content_2")
                 .build();
@@ -37,10 +39,50 @@ class BoardRepositoryTest {
         // given
         Board saveBoard = boardRepository.save(board1);
 
+        log.info("saveBoard :{}", saveBoard);
         // when
 
         // then
         assertThat(saveBoard.getTitle()).isEqualTo("title_1");
         assertThat(saveBoard.getContent()).isEqualTo("content_1");
+    }
+
+    @Test
+    void update() {
+        // given
+        Board saveBoard = boardRepository.save(board1);
+
+        log.info("saveBoard :{}", saveBoard);
+
+        Board updateBoard = Board.builder()
+                .id(1L)
+                .title("update_title")
+                .content("update_content")
+                .build();
+
+        // when
+        boardRepository.save(updateBoard);
+
+        log.info("updateBoard: {}", updateBoard);
+
+        // then
+        assertThat(updateBoard.getTitle()).isEqualTo("update_title");
+    }
+
+    @Test
+    void deleteById() {
+        // given
+        Board saveBoard = boardRepository.save(board1);
+        log.info("saveBoard: {}", saveBoard);
+
+        // when
+        boardRepository.delete(saveBoard);
+        log.info("deleteBoard: {}", saveBoard);
+
+        Optional<Board> deleteBoard = boardRepository.findById(saveBoard.getId());
+        log.info("result :{}", deleteBoard);
+
+        // then
+        assertThat(deleteBoard).isEmpty();
     }
 }
